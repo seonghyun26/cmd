fix - minor bug fix and variance logging

src/generate.py
@@ -26,24 +26,23 @@ def generate(cfg, model_wrapper, epoch, device, logger):
     # Set conditions by task
     task = cfg.job.name
     if task == "simulation":
-        pass
-        # goal_states = load_state_file(cfg, cfg.job.goal_state, device)
+        raise NotImplementedError("Simulation task TBA")
     elif task == "tps":
         goal_states = load_state_file(cfg, cfg.job.goal_state, device)
-        # goal_states *= 1000.0
     else:
         raise ValueError(f"Task {task} not found")
     
     
     # Generate trajectories
     with torch.no_grad():
-        current_states = inital_states
+        current_states = inital_states * 1000.0
+        goal_states *= 1000.0
         for t in tqdm(
             range(time_horizon),
             desc=f"Epoch {epoch}, genearting {sample_num} trajectories for {task}"
         ):
             step = torch.tensor(time_horizon - t).to(current_states.device).repeat(sample_num, 1)
-            states_offset = model_wrapper(
+            states_offset, var = model_wrapper(
                 current_state=current_states,
                 goal_state=goal_states,
                 step=step,
@@ -60,6 +59,7 @@ def generate(cfg, model_wrapper, epoch, device, logger):
     
     
     trajectory_list = torch.stack(state_list, dim=1)
+    trajectory_list /= 1000.0
     if cfg.job.save:
         save_trajectory(cfg, trajectory_list, epoch, logger)
     

src/load.py
@@ -140,7 +140,7 @@ class ModelWrapper(nn.Module):
         torch.save(self.model.state_dict(), f"{path}/model-{epoch}.pt")
         
     def load_from_checkpoint(self, path):
-        self.model.load_state_dict(torch.load(f"{path}/encoder.pt"))
+        self.model.load_state_dict(torch.load(f"{path}/model.pt"))
     
     def forward(self,
         current_state: torch.Tensor,
@@ -165,10 +165,10 @@ class ModelWrapper(nn.Module):
         state_offset = self.reparameterize(mu, var)
         state_offset = state_offset.reshape(batch_size, self.atom_num, 3)
         
-        return state_offset
+        return state_offset, var
     
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
+    def reparameterize(self, mu, var):
+        std = torch.exp(var)
         eps = torch.randn_like(std)
         
         return mu + eps * std
@@ -179,7 +179,6 @@ def load_model_wrapper(cfg, device):
     scheduler = load_scheduler(cfg, optimizer)
     
     return model_wrapper, optimizer, scheduler
-
 
 
 def load_optimizer(cfg, model_param):

src/metric.py
@@ -33,6 +33,7 @@ def compute_thp(cfg, trajectory_list, goal_state):
     molecule = cfg.job.molecule
     sample_num = cfg.job.sample_num
     last_state = trajectory_list[:, -1]
+    cv_bound = cfg.job.thp_cv_bound
     
     if molecule == "alanine":
         phi_angle = [1, 6, 8, 14]
@@ -43,7 +44,7 @@ def compute_thp(cfg, trajectory_list, goal_state):
         phi_goal = compute_dihedral(goal_state[:, phi_angle])
         psi_goal = compute_dihedral(goal_state[:, psi_angle])
         
-        hit = (np.abs(psi - psi_goal) < 0.75) & (np.abs(phi - phi_goal) < 0.75)
+        hit = (np.abs(psi - psi_goal) < cv_bound) & (np.abs(phi - phi_goal) < cv_bound)
     else:
         raise ValueError(f"THP for molecule {molecule} TBA")
     

src/plot.py
@@ -7,18 +7,17 @@ import matplotlib.pyplot as plt
 
 
 def plot_ad_potential(potential, traj_dihedral, start_dihedral, goal_dihedral):
-    # Plot the potential surface    
     plt.clf()
     fig = plt.figure(figsize=(7, 7))
     ax = fig.add_subplot(111)
     sample_num = traj_dihedral[0].shape[0]
     traj_length = traj_dihedral[0].shape[1]
 
+    # Plot the potential
     xs = np.arange(-np.pi, np.pi + 0.1, 0.1)
     ys = np.arange(-np.pi, np.pi + 0.1, 0.1)
     x, y = np.meshgrid(xs, ys)
     inp = torch.tensor(np.array([x, y])).view(2, -1).T
-
     z = potential.potential(inp)
     z = z.view(y.shape[0], y.shape[1])
     plt.contourf(xs, ys, z, levels=100, zorder=0)