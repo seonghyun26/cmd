fix - bug fix, computation speed up with torch

main.py
@@ -146,19 +146,30 @@ def main(cfg):
     else:
         # Load trainined model from checkpoint
         epoch = 0
-        model_wrapper.load_from_checkpoint(f"./model/{cfg.job.molecule}/{cfg.model.name}/{cfg.training.ckpt_file}.pt")
-        model_wrapper.eval()
+        if cfg.job.generate:
+            model_wrapper.load_from_checkpoint(f"./model/{cfg.job.molecule}/{cfg.model.name}/{cfg.training.ckpt_file}.pt")
+            model_wrapper.eval()
+        else:
+            logger.info(f"Using generated trajectories from {trajectory_dir}")
 
     # Test model on downstream task (generation)
     if cfg.job.evaluate:
         logger.info("Evaluating...")
-        trajectory_list = generate(
-            cfg=cfg,
-            model_wrapper=model_wrapper,
-            epoch=epoch,
-            device=device,
-            logger=logger
-        )
+        if cfg.job.generate:
+            trajectory_list = generate(
+                cfg=cfg,
+                model_wrapper=model_wrapper,
+                epoch=epoch,
+                device=device,
+                logger=logger
+            )
+        else:
+            trajectory_dir = cfg.job.traj_dir
+            trajectory_list = []
+            for i in range(cfg.job.sample_num):
+                trajectory = np.load(f"{trajectory_dir}/{i}.npy")
+                trajectory_list.append(trajectory)
+            trajectory_list = np.array(trajectory_list)
         evaluate(
             cfg=cfg,
             model_wrapper=model_wrapper,

src/evaluate.py
@@ -46,12 +46,15 @@ def evaluate(cfg, model_wrapper, trajectory_list, logger, epoch, device):
 
 
 def evaluate_tps(cfg, model_wrapper, trajectory_list, logger, epoch, device):
-    # Trajectory list shape: (sample_num, time_horizon, atom_num, 3)
+    '''
+        Trajectory: torch.Tensor (sample_num, time_horizon, atom_num, 3)
+    '''
+    
     goal_state = load_state_file(cfg, cfg.job.goal_state, device)
-    goal_state = goal_state.to("cpu")
+    goal_state = goal_state.to(device)
     if isinstance(trajectory_list, np.ndarray):
         trajectory_list = torch.tensor(trajectory_list, dtype=torch.float32, device=device)
-    trajectory_list = trajectory_list.to("cpu")
+    trajectory_list = trajectory_list.to(device)
     eval_result = {}
     
     if cfg.job.metrics.thp.use:
@@ -65,6 +68,12 @@ def evaluate_tps(cfg, model_wrapper, trajectory_list, logger, epoch, device):
             eval_result["eval/epd"], eval_result["eval/rmsd"]  = None, None
         else:
             eval_result["eval/epd"], eval_result["eval/rmsd"]  = compute_epd(cfg, trajectory_list, goal_state, hit_mask, hit_index)
+    if cfg.job.metrics.ram.use:
+        logger.info(">> Plotting paths")
+        eval_result["eval/ram"], eval_result["eval/transition_path"] = compute_ram(cfg, trajectory_list, hit_mask, hit_index, epoch)
+    if cfg.job.metrics.projection.use:
+        logger.info(">> Plotting projected CV values")
+        eval_result["eval/projection"], eval_result["eval/state"], eval_result["eval/contour"] = compute_projection(cfg, model_wrapper, epoch)
     if cfg.job.metrics.energy.use:
         if hit_mask.sum() == 0:
             logger.info("No hit found, skipping energy computation")
@@ -72,12 +81,6 @@ def evaluate_tps(cfg, model_wrapper, trajectory_list, logger, epoch, device):
         else:
             logger.info(">> Computing Energy")
             eval_result["eval/max_energy"], eval_result["eval/final_energy"], eval_result["eval/final_energy_err"] = compute_energy(cfg, trajectory_list, goal_state, hit_mask, hit_index)
-    if cfg.job.metrics.ram.use:
-        logger.info(">> Plotting paths")
-        eval_result["eval/ram"], eval_result["eval/transition_path"] = compute_ram(cfg, trajectory_list, hit_mask, hit_index, epoch)
-    if cfg.job.metrics.projection.use:
-        logger.info(">> Plotting projected CV values")
-        eval_result["eval/projection"], eval_result["eval/state"], eval_result["eval/contour"] = compute_projection(cfg, model_wrapper, epoch)
     # if cfg.job.metrics.jacobian.use:
     #     logger.info(">> Computing Jacobian for mlcv against input")
     #     eval_result["eval/jacobian"] = compute_jacobian(cfg, model_wrapper, epoch)
@@ -97,6 +100,9 @@ def evaluate_cv(cfg, model_wrapper, logger, epoch, device):
     if cfg.job.metrics.projection.use:
         logger.info(">> Plotting projected CV values")
         eval_result["eval/projection"], eval_result["eval/state"], eval_result["eval/contour"] = compute_projection(cfg, model_wrapper, epoch)
+        # if cfg.job.metrics.jacobian.use:
+    #     logger.info(">> Computing Jacobian for mlcv against input")
+    #     eval_result["eval/jacobian"] = compute_jacobian(cfg, model_wrapper, epoch)
     
     for key in eval_result.keys():
         logger.info(f"{key}: {eval_result[key]}")

src/metric.py
@@ -108,6 +108,7 @@ def potential_energy(cfg, trajectory):
 
 
 def compute_thp(cfg, trajectory_list, goal_state):
+    device = trajectory_list.device
     molecule = cfg.job.molecule
     sample_num = cfg.job.sample_num
     cv_bound = cfg.job.metrics.thp.cv_bound
@@ -116,20 +117,21 @@ def compute_thp(cfg, trajectory_list, goal_state):
     hit_index = []
     
     if molecule == "alanine":
-        psi_goal = compute_dihedral(goal_state[0, ALDP_PSI_ANGLE].reshape(-1, len(ALDP_PSI_ANGLE), 3))
-        phi_goal = compute_dihedral(goal_state[0, ALDP_PSI_ANGLE].reshape(-1, len(ALDP_PHI_ANGLE), 3))
+        psi_goal = compute_dihedral_torch(goal_state[0, ALDP_PSI_ANGLE].reshape(-1, len(ALDP_PSI_ANGLE), 3))
+        phi_goal = compute_dihedral_torch(goal_state[0, ALDP_PHI_ANGLE].reshape(-1, len(ALDP_PHI_ANGLE), 3))
         for i in tqdm(
             range(sample_num),
-            desc = "Computing THP for trajectories"
+            desc = f"Computing THP for {trajectory_list.shape[0]} trajectories"
         ):
-            psi = compute_dihedral(trajectory_list[i, :, ALDP_PSI_ANGLE])
-            phi = compute_dihedral(trajectory_list[i, :, ALDP_PHI_ANGLE])
-            psi_hit_distance = np.abs(psi - psi_goal)
-            phi_hit_distance = np.abs(phi - phi_goal)
-            cv_distance = np.sqrt(psi_hit_distance ** 2 + phi_hit_distance ** 2)
+            psi = compute_dihedral_torch(trajectory_list[i, :, ALDP_PSI_ANGLE])
+            phi = compute_dihedral_torch(trajectory_list[i, :, ALDP_PHI_ANGLE])
+            psi_hit_distance = torch.abs(psi - psi_goal)
+            phi_hit_distance = torch.abs(phi - phi_goal)
+            cv_distance = torch.sqrt(psi_hit_distance ** 2 + phi_hit_distance ** 2)
             hit_in_path = (psi_hit_distance < cv_bound) & (phi_hit_distance < cv_bound)
-            hit_index_in_path = np.argmin(cv_distance)
-            if np.any(hit_mask):
+            hit_index_in_path = torch.argmin(cv_distance)
+            
+            if torch.any(hit_in_path):
                 hit_rate += 1.0
                 hit_mask.append(True)
                 hit_index.append(hit_index_in_path)
@@ -138,15 +140,8 @@ def compute_thp(cfg, trajectory_list, goal_state):
                 hit_index.append(-1)
                 
         hit_rate /= sample_num
-        hit_mask = torch.tensor(hit_mask, dtype=torch.bool, device=trajectory_list.device)
-        hit_index = torch.tensor(hit_index, dtype=torch.int32, device=trajectory_list.device)
-        
-        # phi = compute_dihedral(last_state[:, ALDP_PHI_ANGLE])
-        # psi = compute_dihedral(last_state[:, ALDP_PSI_ANGLE])
-        # phi_goal = compute_dihedral(goal_state[:, ALDP_PHI_ANGLE])
-        # psi_goal = compute_dihedral(goal_state[:, ALDP_PSI_ANGLE])
-        # hit = (np.abs(psi - psi_goal) < cv_bound) & (np.abs(phi - phi_goal) < cv_bound)
-        # hit_rate = hit.sum() / hit.shape[0]
+        hit_mask = torch.tensor(hit_mask, dtype=torch.bool, device=device)
+        hit_index = torch.tensor(hit_index, dtype=torch.int32, device=device)
     elif molecule == "double-well":
         hit = (np.abs(last_state[:, 0] - goal_state[:, 0]) < cv_bound) & (np.abs(last_state[:, 1] - goal_state[:, 1]) < cv_bound)
         hit_rate = torch.all(hit) / hit.shape[0]
@@ -159,22 +154,29 @@ def compute_thp(cfg, trajectory_list, goal_state):
 def compute_epd(cfg, trajectory_list, goal_state, hit_mask, hit_index):
     molecule = cfg.job.molecule
     atom_num = cfg.data.atom
+    sample_num = trajectory_list.shape[0]
     unit_scale_factor = 1000
     hit_trajectory = trajectory_list[hit_mask]
+    hit_path_num = hit_mask.sum().item()
     goal_state = goal_state[hit_mask]
     epd = 0.0
     
     # Compute EPD
     if molecule == "alanine":
-        for i in range(hit_trajectory.shape[0]):
-            hit_state = hit_trajectory[i, hit_index[i]]
-            matrix_f_norm = torch.sqrt(torch.square(
-                pairwise_distance(hit_state, hit_state) - pairwise_distance(goal_state, goal_state)
-            ).sum((1, 2)))
-            epd += matrix_f_norm / (atom_num ** 2) * unit_scale_factor
+        hit_state = []
+        for i in tqdm(
+            range(hit_path_num),
+            desc = f"Computing EPD, RMSD for {hit_path_num} hitting trajectories"
+        ):
+            hit_state.append(hit_trajectory[i, hit_index[i]])
+        hit_state = torch.stack(hit_state)
+        matrix_f_norm = torch.sqrt(torch.square(
+            pairwise_distance(hit_state, hit_state) - pairwise_distance(goal_state, goal_state)
+        ).sum((1, 2)))
+        epd = torch.mean(matrix_f_norm / (atom_num ** 2) * unit_scale_factor)
     else:
         raise ValueError(f"EPD for molecule {molecule} TBA")
-    epd /= hit_trajectory.shape[0]
+    # epd /= hit_trajectorsy.shape[0]
     
     # TODO: RMSD computation for alanine
     if molecule == "alanine":
@@ -227,20 +229,22 @@ def compute_energy(cfg, trajectory_list, goal_state, hit_mask, hit_index):
 
 def compute_ram(cfg, trajectory_list, hit_mask, hit_index, epoch):
     molecule = cfg.job.molecule
+    hit_path_num = hit_mask.sum().item()
+    
     if molecule == "alanine":
         # Load start, goal state and compute phi, psi
         start_state_xyz = md.load(f"./data/{cfg.job.molecule}/{cfg.job.start_state}.pdb").xyz
         goal_state_xyz = md.load(f"./data/{cfg.job.molecule}/{cfg.job.goal_state}.pdb").xyz
         start_state = torch.tensor(start_state_xyz)
         goal_state = torch.tensor(goal_state_xyz)
-        phi_start = compute_dihedral(start_state[:, ALDP_PHI_ANGLE])
-        psi_start = compute_dihedral(start_state[:, ALDP_PSI_ANGLE])
-        phi_goal = compute_dihedral(goal_state[:, ALDP_PHI_ANGLE])
-        psi_goal = compute_dihedral(goal_state[:, ALDP_PSI_ANGLE])
+        phi_start = compute_dihedral_torch(start_state[:, ALDP_PHI_ANGLE])
+        psi_start = compute_dihedral_torch(start_state[:, ALDP_PSI_ANGLE])
+        phi_goal = compute_dihedral_torch(goal_state[:, ALDP_PHI_ANGLE])
+        psi_goal = compute_dihedral_torch(goal_state[:, ALDP_PSI_ANGLE])
     
         # Compute phi, psi from trajectory_list
-        phi_traj_list = np.array([compute_dihedral(trajectory[:, ALDP_PHI_ANGLE]) for trajectory in trajectory_list])
-        psi_traj_list = np.array([compute_dihedral(trajectory[:, ALDP_PSI_ANGLE]) for trajectory in trajectory_list])
+        phi_traj_list = [compute_dihedral_torch(trajectory[:, ALDP_PHI_ANGLE]) for trajectory in trajectory_list]
+        psi_traj_list = [compute_dihedral_torch(trajectory[:, ALDP_PSI_ANGLE]) for trajectory in trajectory_list]
         
         ram_plot_img = plot_ad_potential(
             traj_dihedral = (phi_traj_list, psi_traj_list),
@@ -248,19 +252,26 @@ def compute_ram(cfg, trajectory_list, hit_mask, hit_index, epoch):
             goal_dihedral = (phi_goal, psi_goal),
             cv_bound_use = cfg.job.metrics.projection.bound_use,
             cv_bound = cfg.job.metrics.thp.cv_bound,
+            hit_path_num = hit_path_num,
             epoch = epoch,
             name = "paths"
         )
         
-        transition_path_plot_img = plot_ad_potential(
-            traj_dihedral = (phi_traj_list[hit_mask], psi_traj_list[hit_mask]),
-            start_dihedral = (phi_start, psi_start),
-            goal_dihedral = (phi_goal, psi_goal),
-            cv_bound_use = cfg.job.metrics.projection.bound_use,
-            cv_bound = cfg.job.metrics.thp.cv_bound,
-            epoch = epoch,
-            name = "transition_paths"
-        )
+        if hit_mask.sum() == 0:
+            transition_path_plot_img = None
+        else:
+            hit_phi_traj_list = [phi_traj_list[i][:hit_index[i]] for i in range(len(phi_traj_list)) if hit_mask[i]]
+            hit_psi_traj_list = [psi_traj_list[i][:hit_index[i]] for i in range(len(psi_traj_list)) if hit_mask[i]]
+            transition_path_plot_img = plot_ad_potential(
+                traj_dihedral = (hit_phi_traj_list, hit_psi_traj_list),
+                start_dihedral = (phi_start, psi_start),
+                goal_dihedral = (phi_goal, psi_goal),
+                cv_bound_use = cfg.job.metrics.projection.bound_use,
+                cv_bound = cfg.job.metrics.thp.cv_bound,
+                hit_path_num = hit_path_num,
+                epoch = epoch,
+                name = "transition_paths"
+            )
         
     elif molecule == "double-well":
         device = trajectory_list.device

src/plot.py
@@ -133,14 +133,16 @@ def plot_ad_potential(
     goal_dihedral,
     cv_bound_use,
     cv_bound,
+    hit_path_num,
     epoch,
     name
 ):
     plt.clf()
     fig = plt.figure(figsize=(7, 7))
     ax = fig.add_subplot(111)
-    sample_num = traj_dihedral[0].shape[0]
-    traj_length = traj_dihedral[0].shape[1]
+    sample_num = len(traj_dihedral[0])
+    traj_list_phi = traj_dihedral[0]
+    traj_list_psi = traj_dihedral[1]
 
     # Plot the potential
     xs = np.arange(-np.pi, np.pi + 0.1, 0.1)
@@ -155,12 +157,12 @@ def plot_ad_potential(
     # Plot the trajectory
     cm = plt.get_cmap("gist_rainbow")
     ax.set_prop_cycle(
-        color=[cm(1.0 * i / sample_num) for i in range(sample_num)]
+        color=[cm(1.0 * i / sample_num) for i in range(hit_path_num)]
     )
-    for idx in range(sample_num):
+    for idx in range(hit_path_num):
         ax.plot(
-            traj_dihedral[0][idx],
-            traj_dihedral[1][idx],
+            traj_list_phi[idx].cpu(),
+            traj_list_psi[idx].cpu(),
             marker="o",
             linestyle="None",
             markersize=3,
@@ -179,7 +181,7 @@ def plot_ad_potential(
         square = plt.Rectangle(
             (goal_dihedral[0] - cv_bound / 2, goal_dihedral[1] - cv_bound /2),
             cv_bound, cv_bound,
-            color='r', fill=False, linewidth=2,
+            color='r', fill=False, linewidth=4,
             zorder=101
         )
         plt.gca().add_patch(square)
@@ -203,7 +205,6 @@ def plot_dw_potential(traj, start, goal, epoch):
     fig = plt.figure(figsize=(7, 7))
     ax = fig.add_subplot(111)
     sample_num = traj.shape[0]
-    traj_length = traj.shape[1]
 
     # Plot the potential
     bound = 1.7